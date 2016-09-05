16eb8cda366f702f47b38dbcf4b9c13f939b1103
Eugen Dahm <eugen.dahm@gmail.com>
2016-09-05 18:40:37 +0200

    add base and base-devel to install packages file

--- package-install.sh
+++ package-install.sh
@@ -8,3 +8,7 @@ pacman.conf /etc/pacman.conf
 
 #install packages with pacstrap
 cat $PACKAGE_LIST_INSTALL | xargs pacstrap /mnt
+
+
+# copy yaourt package list to /mnt/tmp
+cp $PACKAGE_LIST_DIR/arch_packages_aur.txt /mnt/tmp
