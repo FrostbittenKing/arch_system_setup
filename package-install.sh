3c39e090ac194b35c6a0cf246ba7208571fcb037
Eugen Dahm <eugen.dahm@gmail.com>
2016-09-05 21:21:03 +0200

    add pacman command to install unzip

--- package-install.sh
+++ package-install.sh
@@ -5,12 +5,16 @@ PACKAGE_LIST_DIR=packages
 PACKAGE_LIST_INSTALL=$PACKAGE_LIST_DIR/arch_packages_install.txt
 ROOT=/mnt
 ARCH_SETUP_ZIP=https://github.com/FrostbittenKing/arch_system_setup/archive/master.zip
+
+# needed packages
+pacman -Syu --noconfirm
+pacman -S unzip
 # fetch install package
 wget $ARCH_SETUP_ZIP -O master.zip
 mkdir /installer
 unzip master.zip /installer
 # copy pacman.conf
-wget /installer/master/conf/pacman.conf -O /etc/pacman.conf
+cp /installer/master/conf/pacman.conf /etc/pacman.conf
 
 #install packages with pacstrap
 cat /installer/master/$PACKAGE_LIST_INSTALL | xargs pacstrap /mnt
