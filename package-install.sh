fb0a90d172a781a150926476f15f2d76c5f28e57
Eugen Dahm <eugen.dahm@gmail.com>
2016-09-05 18:15:22 +0200

    add pacman.conf and overwrite install scripts pacman.conf

--- package-install.sh
+++ package-install.sh
@@ -3,11 +3,8 @@
 PACKAGE_LIST_DIR=packages
 PACKAGE_LIST_INSTALL=$PACKAGE_LIST_DIR/arch_packages_install.txt
 
-#add archlinuxfr to pacman.conf
-echo -e '[archlinuxfr]
-SigLevel = PackageOptional
-Server = http://repo.archlinux.fr/$arch
-'>> /etc/pacman.conf
+# copy pacman.conf
+pacman.conf /etc/pacman.conf
 
 #install packages with pacstrap
 cat $PACKAGE_LIST_INSTALL | xargs pacstrap /mnt
