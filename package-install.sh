78f064794d1241c77b57408b1cb727ebc8e40eb6
Eugen Dahm <eugen.dahm@gmail.com>
2016-09-05 21:10:36 +0200

    better install process script

--- package-install.sh
+++ package-install.sh
@@ -5,14 +5,19 @@ PACKAGE_LIST_DIR=packages
 PACKAGE_LIST_INSTALL=$PACKAGE_LIST_DIR/arch_packages_install.txt
 ROOT=/mnt
 ARCH_SETUP_ZIP=https://github.com/FrostbittenKing/arch_system_setup/archive/master.zip
+# fetch install package
+wget $ARCH_SETUP_ZIP -O master.zip
+mkdir /installer
+unzip master.zip /installer
 # copy pacman.conf
-$CONF_DIR/pacman.conf /etc/pacman.conf
+wget /installer/master/conf/pacman.conf -O /etc/pacman.conf
 
 #install packages with pacstrap
-cat $PACKAGE_LIST_INSTALL | xargs pacstrap /mnt
+cat /installer/master/$PACKAGE_LIST_INSTALL | xargs pacstrap /mnt
 
 genfstab -U $ROOT >> $ROOT/etc/fstab
 
 # download arch_system_setup
-wget $ARCH_SETUP_ZIP -O $ROOT/tmp
+#wget $ARCH_SETUP_ZIP -O $ROOT/tmp
+cp -r /installer/master $ROOT/TMP
 arch-chroot
