5d871b230e296bb8de516c28b4bafba12e3adb62
Eugen Dahm <eugen.dahm@gmail.com>
2016-09-05 21:28:41 +0200

    fix directory errors

--- package-install.sh
+++ package-install.sh
@@ -5,19 +5,20 @@ PACKAGE_LIST_DIR=packages
 PACKAGE_LIST_INSTALL=$PACKAGE_LIST_DIR/arch_packages_install.txt
 ROOT=/mnt
 ARCH_SETUP_ZIP=https://github.com/FrostbittenKing/arch_system_setup/archive/master.zip
-
+SCRIPT_DIR_ROOT=/installer
+INSTALLER_DIR=/$SCRIPT_DIR_ROOT/arch_system_setup-master
 # needed packages
 pacman -Syu --noconfirm
 pacman -S unzip
 # fetch install package
 wget $ARCH_SETUP_ZIP -O master.zip
-mkdir /installer
-unzip master.zip /installer
+mkdir $SCRIPT_DIR_ROOT
+unzip master.zip -d $SCRIPT_DIR_ROOT
 # copy pacman.conf
-cp /installer/master/conf/pacman.conf /etc/pacman.conf
+cp $INSTALLER_DIR/conf/pacman.conf /etc/pacman.conf
 
 #install packages with pacstrap
-cat /installer/master/$PACKAGE_LIST_INSTALL | xargs pacstrap /mnt
+cat $INSTALLER_DIR/$PACKAGE_LIST_INSTALL | xargs pacstrap /mnt
 
 genfstab -U $ROOT >> $ROOT/etc/fstab
 
