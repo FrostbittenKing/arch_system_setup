3989d5fc812c5aeff73a8542da112c0484383ad1
Eugen Dahm <eugen.dahm@gmail.com>
2016-09-05 20:01:36 +0200

    better structuring

--- package-install.sh
+++ package-install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+CONF_DIR=conf
 PACKAGE_LIST_DIR=packages
 PACKAGE_LIST_INSTALL=$PACKAGE_LIST_DIR/arch_packages_install.txt
-
+ROOT=/mnt
+ARCH_SETUP_ZIP=https://github.com/FrostbittenKing/arch_system_setup/archive/master.zip
 # copy pacman.conf
-pacman.conf /etc/pacman.conf
+$CONF_DIR/pacman.conf /etc/pacman.conf
 
 #install packages with pacstrap
 cat $PACKAGE_LIST_INSTALL | xargs pacstrap /mnt
 
+genfstab -U $ROOT >> $ROOT/etc/fstab
 
-# copy yaourt package list to /mnt/tmp
-cp $PACKAGE_LIST_DIR/arch_packages_aur.txt /mnt/tmp
+# download arch_system_setup
+wget $ARCH_SETUP_ZIP -O $ROOT/tmp
+arch-chroot
