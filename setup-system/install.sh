fc754e481a846582e765747d4ad9242fb851eca7
Eugen Dahm <eugen.dahm@gmail.com>
2016-09-07 17:55:02 +0200

    extract some strings into variables, prompt for username instead of hardcoded etc

--- setup-system/install.sh
+++ setup-system/install.sh
@@ -5,6 +5,10 @@ INSTALLER_DIR=/$SCRIPT_DIR_ROOT/arch_system_setup-master
 PACKAGE_LIST_DIR=$INSTALLER_DIR/packages
 PACKAGE_LIST_AUR=$PACKAGE_LIST_DIR/arch_packages_aur.txt
 
+# list of services to enable
+SERVICE_LIST="dhcpcd.service NetworkManager.service"
+# change to your favorite Display manager
+DM="slim.service"
 # configure timezone
 ln -s /usr/share/zoneinfo/Europe/Vienna /etc/localtime
 # not sure about that
@@ -15,11 +19,15 @@ sed -i 's/#de_AT.UTF-8 UTF-8/de_AT.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 # enable services
-systemctl enable dhcpcd.service slim.service
+systemctl enable $SERVICE_LIST
+
+#enable display manager
+systemctl enable $DM
 
 # create initramfs
 mkinitcpio -p linux
-passwd
+
+echo "enter new root password";passwd
 
 # set default font to ttf-droid
 # ln -s /etc/fonts/conf.avail/60-ttf-droid-sans-mono-fontconfig.conf /etc/fonts/conf.d/
@@ -28,11 +36,13 @@ passwd
 # ln -s /etc/fonts/conf.avail/65-ttf-droid-serif-fontconfig.conf /etc/fonts/conf.d/
 
 # create user
-useradd -m -G disk,wheel,uucp,games,lock,kvm,video -s /usr/bin/zsh itachi
-echo 'get PASSWORD itachi'
-passwd itachi
+echo "please enter a username for your account: "; read username
+useradd -m -G disk,wheel,uucp,games,lock,kvm,video -s /usr/bin/zsh $username
+echo "get PASSWORD $username: "; passwd $username
 
-su - itachi <<'EOF'
+# enable sudo
+read -p "uncomment wheel group in /etc/sudoers"; visudo
+su - $username <<'EOF'
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -44,7 +54,7 @@ git config --global color.status "auto"
 git config --global color.branch "auto"
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 git config --global core.editor "emacs -nw"
-git config --global core.excludefile "/home/itachi/.gitignore"
+git config --global core.excludefile "$HOME/.gitignore"
 git config --global github.user "FrostbittenKing"
 git config --global tar.tar.xz.command "xz -T0 -c"
 git config --global push.default "current"
@@ -59,6 +69,7 @@ yaourt -S --noconfirm $(cat $PACKAGE_LIST_AUR)
 EOF
 
 # TODO configs
+# maybe fetch from its own repository, idk
 # slim.conf
 # .xinitrc
 # .Xresources
