d45a3b8624e4f9c094be6404daa2fa9a7f3952eb
Eugen Dahm <eugen.dahm@gmail.com>
2025-12-20 11:17:11 +0100

    Fix hooks and track installation status
    * Fix mkinitcpio hooks for disk layout
    * Create install tracking mechanism for continuation

--- package-install.sh
+++ package-install.sh
@@ -7,23 +7,27 @@ ARCH_SETUP_TAR=master.tar.gz
 SCRIPT_DIR_ROOT=/installer
 INSTALLER_DIR=$SCRIPT_DIR_ROOT/arch_system_setup-master
 ANSWER_FILE=arch_answers.txt
-
+INSTALL_STATUS=installation_status.txt
 function init_setup {
     cat <<EOF > $ANSWER_FILE
 ARCH_SETUP_TAR_URL=https://github.com/FrostbittenKing/arch_system_setup/archive/refs/heads/$ARCH_SETUP_TAR
 ROOT=/mnt
 SYSTEM_SETUP_DIR=$ROOT/arch_system_setup-master/setup-system
 # list of services to enable
-SERVICE_LIST="dhcpcd.service NetworkManager.service"
+SERVICE_LIST="NetworkManager.service"
 # change to your favorite Display manager
 DM="lxdm.service"
 TIMEZONE=Europe/Vienna
-LOCALIZATIONS=("en_US.UTF-8 UTF-8" "de_AT.UTF-8 UTF-8")
+LOCALIZATIONS=("en_US.UTF-8 UTF-8")
 SYS_LANGUAGE='LANG=en_US.UTF-8'
 USERNAME=itachi
 DEFAULT_SHELL=/usr/bin/zsh
 EXTERN_CONFIGS_GIT=(https://github.com/FrostbittenKing/awesome-wm-config.git)
 INSTALL_OPTIONAL_PACKAGES=1
+EOF
+    echo "removing install status file"
+    cat <<EOF > $INSTALL_STATUS
+ANSWER_FILE_WRITTEN=true
 EOF
     echo "Answer File $ANSWER_FILE written, please customize it to suit your purpose or restart this shell script"
 }
@@ -31,8 +35,24 @@ EOF
 function copy_cfg_to_target {
     cp -r $INSTALLER_DIR $ROOT
     cp $ANSWER_FILE $ROOT
+    echo "CFG_COPIED_TO_TARGET=true" >> $INSTALL_STATUS
+    cp $INSTALL_STATUS $ROOT
 }
 
+function get_and_extract_install_archive {
+    # fetch install package
+    curl -L $ARCH_SETUP_TAR_URL -o $ARCH_SETUP_TAR
+    if [ ! -d $SCRIPT_DIR_ROOT ]; then
+	mkdir -p $SCRIPT_DIR_ROOT
+	tar -xzf $ARCH_SETUP_TAR -C $SCRIPT_DIR_ROOT
+    fi
+    echo "ARCHIVE_FETCHED=true" >> $INSTALL_STATUS
+}
+
+function pacstrap_step {
+    cat $INSTALLER_DIR/$PACKAGE_LIST_INSTALL | xargs pacstrap $ROOT
+    echo "PACSTRAPPED=true" >> $INSTALL_STATUS
+}
 # test for initialized answer file
 if [ ! -f $ANSWER_FILE ]; then
     init_setup
@@ -41,17 +61,19 @@ fi
 
 . $ANSWER_FILE
 
-# fetch install package
-curl -L $ARCH_SETUP_TAR_URL -o $ARCH_SETUP_TAR
-mkdir -p $SCRIPT_DIR_ROOT
-tar -xzf $ARCH_SETUP_TAR -C $SCRIPT_DIR_ROOT
+get_and_extract_install_archive
+
+# TODO: stable version that works over time
 # copy pacman.conf
 # cp $INSTALLER_DIR/conf/pacman.conf /etc/pacman.conf
 
 #install packages with pacstrap
-cat $INSTALLER_DIR/$PACKAGE_LIST_INSTALL | xargs pacstrap $ROOT
+pacstrap_step
 
-genfstab -U $ROOT >> $ROOT/etc/fstab
+if [ ! -f $ROOT/etc/fstab ]; then
+    genfstab -U $ROOT >> $ROOT/etc/fstab
+    echo "fstab written..."
+fi
 
 # copy necessary files to target mount point
 copy_cfg_to_target
--- setup-system/install.sh
+++ setup-system/install.sh
@@ -2,6 +2,7 @@
 
 INSTALLER_DIR=/arch_system_setup-master
 ANSWER_FILE=/arch_answers.txt
+INSTALL_STATUS=/installation_status.txt
 CONF_DIR=$INSTALLER_DIR/conf
 # source answer file
 . $ANSWER_FILE
@@ -9,27 +10,59 @@ CONF_DIR=$INSTALLER_DIR/conf
 # copy user configs
 function copy_system_configs {
     cp -a $CONF_DIR/etc /
-    cp -a $CONF_DIR/pacman.conf /etc
+    # todo fix config
+    # cp -a $CONF_DIR/pacman.conf /etc
+}
+# configure timezone and localizations
+function configure_locale_and_timezone
+{
+    echo "Configure timezone and localization"
+    ln -sv /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+    # not sure about that
+    # hwclock --systohc --utc
+    #enable localizations
+    for l in "${LOCALIZATIONS[@]}"
+    do
+	echo "configure locale.gen"
+	sed -i "s/#$l/$l/g" /etc/locale.gen
+    done
+
+    locale-gen
+
+    if [ ! -f /etc/vconsole.conf ]; then
+	echo 'KEYMAP=us' > /etc/vconsole.conf
+    fi
+    echo $SYS_LANGUAGE > /etc/locale.conf
+    echo "LOCALE_CONFIGURED=true" >> $INSTALL_STATUS
 }
 
+# configure init ramfs
+function configure_initramfs
+{
+    mkinitcpio -p linux
+    echo "INITRAMFS_INITIALIZED=true" >> $INSTALL_STATUS
+}
 
+# configure users
+function configure_users
+{
+    # root pw
+    echo "enter new root password";passwd
+
+    # create user
+    # echo "please enter a username for your account: "; read username
+    useradd -m -G disk,wheel,uucp,games,lock,kvm,video -s $DEFAULT_SHELL $USERNAME
+    echo "get PASSWORD $USERNAME: "; passwd $USERNAME
+    
+    # enable sudo
+    read -p "uncomment wheel group in /etc/sudoers"; visudo
+    echo "USERS_CONFIGURED=true" >> $INSTALL_STATUS
+}
 # configure timezone
-ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-# not sure about that
-# hwclock --systohc --utc
-#enable localizations
-for l in "${LOCALIZATIONS[@]}"
-do
-    sed -i "s/#$l/$l/g" /etc/locale.gen
-done
+configure_locale_and_timezone
 
-locale-gen
-
-echo $SYS_LANGUAGE > /etc/locale.conf
 # create initramfs
-mkinitcpio -p linux
-
-echo "enter new root password";passwd
+configure_initramfs
 
 # set default font to ttf-droid
 # ln -s /etc/fonts/conf.avail/60-ttf-droid-sans-mono-fontconfig.conf /etc/fonts/conf.d/
@@ -37,46 +70,50 @@ echo "enter new root password";passwd
 # ln -s /etc/fonts/conf.avail/65-ttf-droid-sans-fontconfig.conf /etc/fonts/conf.d/
 # ln -s /etc/fonts/conf.avail/65-ttf-droid-serif-fontconfig.conf /etc/fonts/conf.d/
 
-# create user
-# echo "please enter a username for your account: "; read username
-useradd -m -G disk,wheel,uucp,games,lock,kvm,video -s $DEFAULT_SHELL $USERNAME
-echo "get PASSWORD $USERNAME: "; passwd $USERNAME
-
-# enable sudo
-read -p "uncomment wheel group in /etc/sudoers"; visudo
+configure_users
 
 copy_system_configs
+
 export INSTALLER_DIR ANSWER_FILE CONF_DIR
-su $USERNAME <<'EOF'
-. $ANSWER_FILE
 
-function copy_git_configs {
-    for grepo in "${EXTERN_CONFIGS_GIT}"
-    do
-	git clone $grepo
-	repo_name=${grepo##*/}
-	repo_dir=${repo_name%.*}
-	cd $repo_dir
-	make install
-	cd ..
-	rm -rf $repo_dir
-    done
+function copy_my_configs
+{
+    su $USERNAME <<'EOF'
+    . $ANSWER_FILE
+
+    function copy_git_configs {
+        if [ ${#EXTERN_CONFIGS_GIT[@]} -eq 0 ]; then
+            return 0
+        fi
+        for grepo in "${EXTERN_CONFIGS_GIT}"
+        do
+	    git clone $grepo
+	    repo_name=${grepo##*/}
+            repo_dir=${repo_name%.*}
+            cd $repo_dir
+            make install
+            cd ..
+            rm -rf $repo_dir
+        done
+    }
+
+    function copy_user_configs {
+        cp -a $CONF_DIR/h/. $HOME
+        copy_git_configs
+        # chown -R $USERNAME.$USERNAME /home/$USERNAME
+    }
+
+    cd /tmp 
+    copy_user_configs
+    cd $HOME
+    # create .zlogin file for last installation steps
+    echo '$HOME/setup-complete.sh' >> .zlogin
+    cp $INSTALLER_DIR/setup-system/setup-complete.sh .
+    chmod +x setup-complete.sh
+    EOF
+    echo "MY_CONFIGS_COPIED=true" >> $INSTALL_STATUS
 }
-
-function copy_user_configs {
-    cp -a $CONF_DIR/h/. $HOME
-    copy_git_configs
-#    chown -R $USERNAME.$USERNAME /home/$USERNAME
-}
-
-cd /tmp 
-copy_user_configs
-cd $HOME
-# create .zlogin file for last installation steps
-echo '$HOME/setup-complete.sh' >> .zlogin
-cp $INSTALLER_DIR/setup-system/setup-complete.sh .
-chmod +x setup-complete.sh
-EOF
+copy_my_configs
 
 # enable services
 systemctl enable $SERVICE_LIST
