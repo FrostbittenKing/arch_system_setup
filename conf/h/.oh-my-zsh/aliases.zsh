11a269c505ba432f4be2b8439bb3a72bcd896cad
Eugen Dahm <eugen.dahm@gmail.com>
2026-01-02 12:18:37 +0100

    Remove some configs to be moved to yadm

--- conf/h/.oh-my-zsh/aliases.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
-alias xz='xz -T0'
-alias nautilus='nautilus --no-desktop'
-alias arduino='_JAVA_AWT_WM_NONREPARENTING= arduino'
-alias less=`findlesssh`
-alias grep="/usr/bin/grep $GREP_OPTIONS"
--- conf/h/.oh-my-zsh/plugins/helpers/helpers.plugin.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-# vim less.sh find helper
-function findlesssh() {
-    find /usr/share/vim -type f -name less.sh
-}
